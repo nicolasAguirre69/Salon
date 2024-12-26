315c0f013afa9a251ff8140e45a949b4d39425b1
nicolas Aguirre69 <angelnicolasaguirre3@gmail.com>
2024-12-26 12:09:08 -0500

    se agrega cconexion a bases de datos

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+    header("content-Type:aplication/json");
+
+    $pdo = new PDO("mysql:host=localhost;dbname=calendario","root","");
+    $consulta = $pdo->prepare("SELECT * FROM calendario");
+    $consulta -> execute();
+
+    $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    print_r(json_encode($resultado));
+
+?>
